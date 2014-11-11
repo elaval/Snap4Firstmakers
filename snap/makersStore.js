b4cfee6d591642177a3bf48a65f57742648f7f7f
Ernesto Laval <ernesto.laval@gmail.com>
2014-11-11 10:31:40 +0000

    Keeps right watcher label (fixes #4)
    Watcher labels disappears when language was changed

---
+++ snap/makersStore.js
@@ -0,0 +1,8 @@
+SnapSerializer.prototype.watcherLabels["makersTemperature"]="temperature";
+SnapSerializer.prototype.watcherLabels["makersLight"]="light";
+SnapSerializer.prototype.watcherLabels["makersAudio"]="audio";
+SnapSerializer.prototype.watcherLabels["makersHumidity"]="humidity";
+SnapSerializer.prototype.watcherLabels["makersInfrared"]="infrared";
+SnapSerializer.prototype.watcherLabels["makersPotentiometer"]="potentiometer";
+SnapSerializer.prototype.watcherLabels["makersSwitch"]="switch";
+
